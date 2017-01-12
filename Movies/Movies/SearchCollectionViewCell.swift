b483465b26e6d6b99d0a4b53e5b3e50c3bbf11f2
Forrest Zhao <fozhao@Forrests-MacBook-Pro.local>
2017-01-12 12:04:19 -0800

    create xib and class for custom search collection view cell, create view model for search view

---
+++ Movies/Movies/SearchCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SearchCollectionViewCell.swift
+//  Movies
+//
+//  Created by Forrest Zhao on 1/11/17.
+//  Copyright © 2017 Forrest Zhao. All rights reserved.
+//
+
+import UIKit
+
+class SearchCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var moviePostImage: UIImageView!
+    @IBOutlet weak var title: UILabel!
+
+}
---
+++ Movies/Movies/SearchViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SearchViewModel.swift
+//  Movies
+//
+//  Created by Forrest Zhao on 1/12/17.
+//  Copyright © 2017 Forrest Zhao. All rights reserved.
+//
+
+import Foundation
