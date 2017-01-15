4827fc153c28b698572eb48247c4a782882f27f9
Forrest Zhao <fozhao@Forrests-MacBook-Pro.local>
2017-01-15 11:04:29 -0800

    impl prepare layout func

--- Movies/Movies/SearchLayout.swift
+++ Movies/Movies/SearchLayout.swift
@@ -24,4 +24,45 @@ class SearchLayout: UICollectionViewLayout {
         let insets = collectionView!.contentInset
         return (collectionView?.bounds)!.width - (insets.left + insets.right)
     }
+    
+    override func prepare() {
+        if cache.isEmpty {
+            // 2
+            let columnWidth = contentWidth / CGFloat(numberOfColumns)
+            var xOffset = [CGFloat]()
+            for column in 0 ..< numberOfColumns {
+                xOffset.append(CGFloat(column) * columnWidth )
+            }
+            var column = 0
+            var yOffset = [CGFloat](repeating: 0, count: numberOfColumns)
+            
+            // 3
+            for item in 0 ..< collectionView!.numberOfItems(inSection: 0) {
+                
+                let indexPath = NSIndexPath(item: item, section: 0)
+                // 4
+                let width = columnWidth - cellPadding * 2
+                let photoHeight = delegate.collectionView(collectionView: collectionView!, heightForPhotoAtIndexPath: indexPath, withWidth: width)
+                let annotationHeight = delegate.collectionView(collectionView: collectionView!, heightForAnnotationAtIndexPath: indexPath, withWidth: width)
+                let height = cellPadding +  photoHeight + annotationHeight + cellPadding
+                let frame = CGRect(x: xOffset[column], y: yOffset[column], width: columnWidth, height: height)
+                let insetFrame = frame.insetBy(dx: cellPadding, dy: cellPadding)
+                
+                // 5
+                let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath as IndexPath)
+                attributes.frame = insetFrame
+                cache.append(attributes)
+                
+                // 6
+                contentHeight = max(contentHeight, frame.maxY)
+                yOffset[column] = yOffset[column] + height
+                
+                if column >= numberOfColumns - 1 {
+                    column = 0
+                } else {
+                    column += 1
+                }
+            }
+        }
+    }
 }
