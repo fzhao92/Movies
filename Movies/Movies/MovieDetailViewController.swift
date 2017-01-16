51500b6e355d029ab020d0851742eaaf986ee0bd
Forrest Zhao <fozhao@Forrests-MacBook-Pro.local>
2017-01-16 12:35:26 -0800

    create detail view

---
+++ Movies/Movies/MovieDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Forrest Zhao on 1/16/17.
+//  Copyright © 2017 Forrest Zhao. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailStackView: UIStackView!
+        @IBOutlet weak var plotLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
