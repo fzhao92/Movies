350897a3a01a7fc857a58d2911b67cfffe153a5a
Forrest Zhao <fozhao@Forrests-MacBook-Pro.local>
2017-01-17 15:38:23 -0800

    populate detail labels

--- Movies/Movies/MovieDetailViewController.swift
+++ Movies/Movies/MovieDetailViewController.swift
@@ -15,20 +15,29 @@ class MovieDetailViewController: UIViewController {
     @IBOutlet weak var plotLabel: UILabel!
     
     var movieID: String?
-    var movieDetailViewModel: MovieDetailViewModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        populateDetailLabels()
+    }
+    
+    func populateDetailLabels() {
         createDetailViewModel(movieID: movieID!) { (movieDetailViewModel) in
-            self.titleLabel.text = movieDetailViewModel.title
+            DispatchQueue.main.async {
+                self.titleLabel.text = movieDetailViewModel.title
+                (self.detailStackView.arrangedSubviews[0] as! UILabel).text = movieDetailViewModel.year
+                (self.detailStackView.arrangedSubviews[1] as! UILabel).text = movieDetailViewModel.rated
+                (self.detailStackView.arrangedSubviews[2] as! UILabel).text = movieDetailViewModel.released
+                (self.detailStackView.arrangedSubviews[3] as! UILabel).text = movieDetailViewModel.genre
+                (self.detailStackView.arrangedSubviews[4] as! UILabel).text = movieDetailViewModel.director
+                (self.detailStackView.arrangedSubviews[5] as! UILabel).text = movieDetailViewModel.actors
+                self.plotLabel.text = movieDetailViewModel.plot
+            }
         }
-        
-        // Do any additional setup after loading the view.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 }
