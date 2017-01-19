62e9fd29179ea1d43f061cf71ac6056a5e401260
Forrest Zhao <fozhao@Forrests-MacBook-Pro.local>
2017-01-18 23:24:59 -0800

    add activity indicator, start on core data stuff

--- Movies/Movies/MovieDetailViewController.swift
+++ Movies/Movies/MovieDetailViewController.swift
@@ -13,6 +13,8 @@ class MovieDetailViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var detailStackView: UIStackView!
     @IBOutlet weak var plotLabel: UILabel!
+    @IBOutlet weak var saveMovieButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     var movieID: String?
     
@@ -21,25 +23,13 @@ class MovieDetailViewController: UIViewController {
         populateDetailLabels()
     }
     
-    func populateDetailLabels() {
-        createDetailViewModel(movieID: movieID!) { (movieDetailViewModel) in
-            DispatchQueue.main.async {
-                self.titleLabel.text = movieDetailViewModel.title
-                (self.detailStackView.arrangedSubviews[0] as! UILabel).text = movieDetailViewModel.year
-                (self.detailStackView.arrangedSubviews[1] as! UILabel).text = movieDetailViewModel.rated
-                (self.detailStackView.arrangedSubviews[2] as! UILabel).text = movieDetailViewModel.released
-                (self.detailStackView.arrangedSubviews[3] as! UILabel).text = movieDetailViewModel.genre
-                (self.detailStackView.arrangedSubviews[4] as! UILabel).text = movieDetailViewModel.director
-                (self.detailStackView.arrangedSubviews[5] as! UILabel).text = movieDetailViewModel.actors
-                self.plotLabel.text = movieDetailViewModel.plot
-            }
-        }
-    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    @IBAction func saveMovieButtonPressed(_ sender: UIButton) {
+        
+    }
 }
 
 //MARK: - Helpers
@@ -56,5 +46,33 @@ extension MovieDetailViewController {
                 completion(MovieDetailViewModel(movieDetail: movieDetail))
             }
         }
+    }
+    
+    func populateDetailLabels() {
+        startActivityIndicator()
+        createDetailViewModel(movieID: movieID!) { (movieDetailViewModel) in
+            DispatchQueue.main.async {
+                self.stopActivityIndicator()
+                self.titleLabel.text = movieDetailViewModel.title
+                (self.detailStackView.arrangedSubviews[0] as! UILabel).text = movieDetailViewModel.year
+                (self.detailStackView.arrangedSubviews[1] as! UILabel).text = movieDetailViewModel.rated
+                (self.detailStackView.arrangedSubviews[2] as! UILabel).text = movieDetailViewModel.released
+                (self.detailStackView.arrangedSubviews[3] as! UILabel).text = movieDetailViewModel.genre
+                (self.detailStackView.arrangedSubviews[4] as! UILabel).text = movieDetailViewModel.director
+                (self.detailStackView.arrangedSubviews[5] as! UILabel).text = movieDetailViewModel.actors
+                self.plotLabel.text = movieDetailViewModel.plot
+            }
+        }
+    }
+    
+    func startActivityIndicator() {
+        activityIndicator.hidesWhenStopped = true
+        saveMovieButton.isEnabled = false
+        activityIndicator.startAnimating()
+    }
+    
+    func stopActivityIndicator() {
+        activityIndicator.stopAnimating()
+        saveMovieButton.isEnabled = true
     }
 }
