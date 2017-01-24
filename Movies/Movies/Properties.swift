61e0ec7d800478bef97e6d079ff0e15b0f48d5c4
Forrest Zhao <fozhao92@gmail.com>
2017-01-23 19:05:16 -0800

    add some colors

---
+++ Movies/Movies/Properties.swift
@@ -0,0 +1,12 @@
+//
+//  Properties.swift
+//  Movies
+//
+//  Created by Forrest Zhao on 1/23/17.
+//  Copyright © 2017 Forrest Zhao. All rights reserved.
+//
+
+import Foundation
+
+enum CollectionViewProperties {
+}
